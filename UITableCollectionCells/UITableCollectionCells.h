5fbd6040fd397ffd8aef8254802ec3810148fbfd
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-03-13 01:36:02 +0800

    added project

---
+++ UITableCollectionCells/UITableCollectionCells.h
@@ -0,0 +1,19 @@
+//
+//  UITableCollectionCells.h
+//  UITableCollectionCells
+//
+//  Created by Lainel John Dela Cruz on 13/03/2019.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITableCollectionCells.
+FOUNDATION_EXPORT double UITableCollectionCellsVersionNumber;
+
+//! Project version string for UITableCollectionCells.
+FOUNDATION_EXPORT const unsigned char UITableCollectionCellsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITableCollectionCells/PublicHeader.h>
+
+
