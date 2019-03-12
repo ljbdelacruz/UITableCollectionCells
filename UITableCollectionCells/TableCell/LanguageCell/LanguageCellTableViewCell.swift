0ba97c8202ab17c3528f46b35ba902d4ca457a8d
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-03-13 01:47:34 +0800

    public language cell

--- UITableCollectionCells/TableCell/LanguageCell/LanguageCellTableViewCell.swift
+++ UITableCollectionCells/TableCell/LanguageCell/LanguageCellTableViewCell.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class LanguageCellTableViewCell: UITableViewCell {
+public class LanguageCellTableViewCell: UITableViewCell {
 
     public static let identifier = "LanguageTableViewCell"
     public static let nibname:String="LanguageCell";
-    
     public var index:Int=0;
     @IBOutlet public weak var UILangLabel: UILabel!
     @IBOutlet public weak var UIFlagImage: UIImageView!
@@ -20,7 +19,6 @@ class LanguageCellTableViewCell: UITableViewCell {
     public override func awakeFromNib(){
         super.awakeFromNib()
         self.Deselect();
-        
     }
     public override func setSelected(_ selected: Bool, animated: Bool){
         super.setSelected(selected, animated: animated)
