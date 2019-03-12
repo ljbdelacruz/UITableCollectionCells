e5ac5d9ae48aa2b7230f1e29a69ac59cec6c15fc
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-03-13 02:25:35 +0800

    added uitableviewcell extension

---
+++ UITableCollectionCells/extension/UITableViewCell+.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewCell+.swift
+//  UITableCollectionCells
+//
+//  Created by Lainel John Dela Cruz on 13/03/2019.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+import Foundation
+
+public extension UITableView {
+    // Rules: nibName == Identifier
+    func registerCustomCell(nibname: String, cellidentifier:String) {
+        let nib = UINib(nibName: nibname, bundle: nil)
+        self.register(nib, forCellReuseIdentifier: cellidentifier)
+    }
+}
